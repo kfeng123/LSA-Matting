04803349a6744945e267bc36d3540dfdb0d4c833
kfeng <446100240@qq.com>
2021-07-27 15:50:11 +0800

    NEW

--- tools/reestimate_foreground.py
+++ tools/reestimate_foreground.py
@@ -9,7 +9,7 @@ import matting.utils.config as config
 if not os.path.exists(config.new_fg_path):
     os.makedirs(config.new_fg_path)
 
-for f in os.listdir(mydir):
+for f in os.listdir(config.fg_path):
     if f.endswith(".jpg") or f.endswith(".png") or f.endswith(".jpeg") or f.endswith(".JPG"):
         img = pymatting.load_image(os.path.join(config.fg_path, f))
         alpha = pymatting.load_image(os.path.join(config.alpha_path, f))
