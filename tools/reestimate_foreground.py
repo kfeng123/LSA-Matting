8343ea61b191a7f6b080671645188bdf64b28f00
kfeng <446100240@qq.com>
2021-07-27 15:52:52 +0800

    NEW

--- tools/reestimate_foreground.py
+++ tools/reestimate_foreground.py
@@ -13,9 +13,9 @@ for f in os.listdir(config.fg_path):
     if f.endswith(".jpg") or f.endswith(".png") or f.endswith(".jpeg") or f.endswith(".JPG"):
         img = pymatting.load_image(os.path.join(config.fg_path, f))
         alpha = pymatting.load_image(os.path.join(config.alpha_path, f))
-        foreground = estimate_foreground_ml(img, alpha)
-        cutout = stack_images(foreground, alpha)
-        save_image(os.path.join(config.new_fg_path, f), cutout)
+        foreground = pymatting.estimate_foreground_ml(img, alpha)
+        cutout = pymatting.stack_images(foreground, alpha)
+        pymatting.save_image(os.path.join(config.new_fg_path, f), cutout)
 
 
 
