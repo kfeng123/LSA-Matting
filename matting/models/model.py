77ffbc1a12912791786c225c0e81b34e532c7d25
kfeng <446100240@qq.com>
2021-08-19 17:52:00 +0800

    NEW

--- matting/models/model.py
+++ matting/models/model.py
@@ -9,7 +9,7 @@ from .backbone.resnet import resnet18, resnet50
 from .backbone.resnet26d import resnet26d
 from .backbone.mmclassification.resnet import ResNetV1d
 from .skip.skip import skipModule_simple, skipModule
-from .decoder.decoder import  decoderModule
+from .decoder.decoder_FAM import  decoderModule
 from .spatial_path.spatial_path import spatial_path
 
 class theModel(nn.Module):
@@ -18,16 +18,16 @@ class theModel(nn.Module):
         self.lastStage = 5
         image_channel = 4
         self.backbone = ResNetV1d(in_channels = image_channel, depth = 50)
-        #self.skip = skipModule_simple(self.backbone._out_feature_channels, lastStage = 5, image_channel = image_channel, ifPPM = True)
-        self.skip = skipModule(self.backbone._out_feature_channels)
+        self.skip = skipModule_simple(self.backbone._out_feature_channels, lastStage = 5, image_channel = image_channel, ifPPM = True)
+        #self.skip = skipModule(self.backbone._out_feature_channels)
         self.decoder = decoderModule(self.skip.outChannels, lastStage = 5, image_channel = image_channel)
-        self.spatial_path = spatial_path()
+        #self.spatial_path = spatial_path()
 
     def forward(self, x):
         encoder_out = self.backbone(x[:,:4,:,:])
         skip_out = self.skip(x, encoder_out)
         decoder_out = self.decoder( skip_out )
-        fine_out = self.spatial_path(x[:,:3,:,:], decoder_out['feature'])
+        #fine_out = self.spatial_path(x[:,:3,:,:], decoder_out['feature'])
         out = {}
         out['alpha'] = fine_out['alpha']
         #out['alpha_coarse'] = decoder_out['alpha_coarse']
